1f16093f85798d1dc78b665a816db7fe53c88381
Noah Cohn <noahc42@gmail.com>
2026-01-07 14:08:42 -0800

    Update build_rubberband.sh

--- emscripten/build_rubberband.sh
+++ emscripten/build_rubberband.sh
@@ -14,14 +14,19 @@ if [ ! -d "rubberband" ]; then
     exit 1
 fi
 
-# We compile all .cpp files in rubberband/src/
+# Define sources: Main wrapper + RubberBand Library implementation
 SOURCES="rubberband/src/*.cpp rubberband_wrapper.cpp"
+
+# NOTE: If RubberBand bundles KissFFT as .c files, we might need to include them.
+# But standard src usually relies on internal or external headers.
+# If you get linker errors about kiss_fft later, we will address that.
 
 emcc -O3 \
     $SOURCES \
     -I . \
     -I rubberband \
     -I rubberband/rubberband \
+    --bind \
     -s WASM=1 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s MODULARIZE=1 \
@@ -29,6 +34,7 @@ emcc -O3 \
     -s EXPORT_NAME='createRubberBandModule' \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
     -s ENVIRONMENT='web,worker' \
+    -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0 \
     -DUSE_KISSFFT \
     -DPROCESS_CMAKE_PROJECT \
     -o ../public/rubberband.js
