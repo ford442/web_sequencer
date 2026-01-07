d9cd8e9d7080124b14da2c8e3a72d550bf39ebe6
Noah Cohn <noahc42@gmail.com>
2026-01-07 14:01:03 -0800

    Update build_rubberband.sh

--- emscripten/build_rubberband.sh
+++ emscripten/build_rubberband.sh
@@ -11,17 +11,15 @@ echo "Compiling Rubber Band WASM..."
 # Check if library exists
 if [ ! -d "rubberband" ]; then
     echo "Error: rubberband directory not found."
-    echo "Please run: git clone https://github.com/breakfastquay/rubberband.git emscripten/rubberband"
     exit 1
 fi
 
-# Define source files. 
 # We compile all .cpp files in rubberband/src/
-# We exclude files that might cause conflicts if they exist (like main.cpp inside src if any)
 SOURCES="rubberband/src/*.cpp rubberband_wrapper.cpp"
 
 emcc -O3 \
     $SOURCES \
+    -I . \
     -I rubberband \
     -I rubberband/rubberband \
     -s WASM=1 \
@@ -30,6 +28,7 @@ emcc -O3 \
     -s EXPORT_ES6=1 \
     -s EXPORT_NAME='createRubberBandModule' \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+    -s ENVIRONMENT='web,worker' \
     -DUSE_KISSFFT \
     -DPROCESS_CMAKE_PROJECT \
     -o ../public/rubberband.js
