94e1eb57dca62ec3284aa1528de17de3128d3e2d
Noah Cohn <noahc42@gmail.com>
2026-02-25 08:44:55 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-  base: "/hyphon/",
+  base: "./",
   plugins: [
     react(),
     wasm(),
