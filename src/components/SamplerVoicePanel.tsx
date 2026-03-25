0a1e564244a4dbce7d5fc5ddaa505ee172a55eb3
Noah Cohn <noahc42@gmail.com>
2026-03-25 03:29:26 -0700

    Update SamplerVoicePanel.tsx

--- src/components/SamplerVoicePanel.tsx
+++ src/components/SamplerVoicePanel.tsx
@@ -2,7 +2,6 @@
 import { Harmonizer, type HarmonizerConfig, type HarmonyType, HARMONIZE_PRESETS } from '../engines/Harmonizer';
 import React, { useState, useCallback } from 'react';
 import { HardwareModule, type KnobConfig } from './HardwareModule';
-import { type HarmonizerConfig } from '../engines/Harmonizer';
 import { LadderButton } from './sampler/LadderButton';
 import { VerticalKnob } from './sampler/VerticalKnob';
 import { HSlider } from './sampler/HSlider';
